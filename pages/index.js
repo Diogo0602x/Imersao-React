47a4be26f772851992b95c87c361430809cbb402
Diogo0602x <diogoteixeira0602@gmail.com>
2022-01-25 14:50:22 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Boas vindas de volta</h1>
+            <h2>Discord - Alura Matrix</h2>
+            <style jsx>{`
+            h1 {
+                color:red;
+            }
+            `}</style>
+        </div>
+    )
+  }
+  
+  export default HomePage
