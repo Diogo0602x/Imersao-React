3e5b1db0e7b47b6618e604cc9072444d52406ed9
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-01-25 23:31:28 -0300

    title

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+function Title() {
+    return (
+        <h1>Boas vindas de volta!</h1>
+    );
+}
+
+//Componente React
 function HomePage() {
+    //JSX
     return (
         <div>
-            <h1>Boas vindas de volta</h1>
+            <Title>Boas vindas de volta!</Title>
             <h2>Discord - Alura Matrix</h2>
             <style jsx>{`
             h1 {
